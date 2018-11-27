fdfb196f52d0431cfed61b3c8b55aa9daf2aefda
Anukriti Goyal <anukritigoyal@D-172-28-102-33.dhcp4.washington.edu>
2018-11-26 18:23:19 -0800

    updated cleaning

--- cleaning.R
+++ cleaning.R
@@ -2,8 +2,13 @@ library(dplyr)
 
 # load in data and split into data frames for male and female athletes
 data <- read.csv('strava_activity.csv')
-data_males <- filter(data, athlete.sex == 'M')
-data_females <- filter(data, athlete.sex == 'F')
 
-write.csv(data_males, file = 'male_activity.csv')
-write.csv(data_females, file = 'female_activity.csv')
+new_data <- data %>% filter(type == "Run")
+new_data <- new_data %>% filter(distance > 400 & distance <= 45000) %>% 
+  filter(average_speed <= 15 & average_speed > 0.1) 
+
+data_males <- new_data %>% filter(athlete.sex == "M")
+
+data_females <- new_data %>% filter(athlete.sex == "F")
+
+
